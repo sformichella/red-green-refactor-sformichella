104e6e7a1cc6068eab8eacb4c8674841a5336d56
sformichella <sformichella@gmail.com>
2020-12-01 16:16:46 -0800

    capAndFilter refactor now uses array methods

--- capitalize-and-filter/capitalize-and-filter.js
+++ capitalize-and-filter/capitalize-and-filter.js
@@ -1,14 +1,7 @@
 const capitalizeAndFilter = arrOfStrings => {
-  const accArr = [];
-
-  for(let i = 0; i < arrOfStrings.length; i++) {
-    const str = arrOfStrings[i].toUpperCase();
-    const firstLetter = str[0];
-
-    if(firstLetter !== 'F') accArr.push(str)
-  }
-
-  return accArr;
+  return arrOfStrings
+    .map(string => string.toUpperCase())
+    .filter(string => string[0] !== 'F');
 }
 
 module.exports = {
