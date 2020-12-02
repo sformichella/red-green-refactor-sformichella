a75b957db71ab60cae046b7289b50db0bc162ca1
sformichella <sformichella@gmail.com>
2020-12-01 16:40:00 -0800

    fetchQuotes gets a quote test

--- fetch-quotes/fetch-quotes.js
+++ fetch-quotes/fetch-quotes.js
@@ -1,5 +1,19 @@
-const fetchQuotes = () => {
+const request = require('superagent');
 
+const fetchQuotes = async () => {
+  const { body } = await request.get('http://futuramaapi.herokuapp.com/api/characters/fry');
+
+  const {
+    character: name,
+    quote: text,
+    image
+  } = body[0];
+  
+  return {
+    name,
+    text,
+    image
+  }
 }
 
 module.exports = {
--- fetch-quotes/fetch-quotes.test.js
+++ fetch-quotes/fetch-quotes.test.js
@@ -1,7 +1,28 @@
 const { fetchQuotes } = require('./fetch-quotes');
+const request = require('superagent');
+
+jest.mock('superagent');
 
 describe('fetchQuotes', () => {
-  it('should get a quote', () => {
-    
+  it('should get a quote in the right format', async() => {
+
+    request.get.mockResolvedValue({
+      body: [
+        {
+          "character": "Fry",
+          "quote": "Not everyone turns out like their parents. Look at me: my parents were honest,\nhard-working people.",
+          "image": "https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904133/fry.png"
+        }
+      ]
+    });
+
+    const quote = await fetchQuotes();
+    const expected = {
+      "name": "Fry",
+      "text": "Not everyone turns out like their parents. Look at me: my parents were honest,\nhard-working people.",
+      "image": "https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904133/fry.png"
+    }
+
+    expect(quote).toEqual(expected);
   });
 });
