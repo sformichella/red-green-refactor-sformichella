9eb2bbbb617ee2314c8e575187287a5c5b1f3110
sformichella <sformichella@gmail.com>
2020-12-01 16:06:59 -0800

    capAndFilter capitalize all strings test

--- capitalize-and-filter/capitalize-and-filter.js
+++ capitalize-and-filter/capitalize-and-filter.js
@@ -1,5 +1,11 @@
 const capitalizeAndFilter = arrOfStrings => {
+  for(let i = 0; i < arrOfStrings.length; i++) {
+    const str = arrOfStrings[i];
 
+    arrOfStrings[i] = str.toUpperCase();
+  }
+
+  return arrOfStrings;
 }
 
 module.exports = {
--- capitalize-and-filter/capitalize-and-filter.test.js
+++ capitalize-and-filter/capitalize-and-filter.test.js
@@ -1,7 +1,12 @@
 const { capitalizeAndFilter } = require('./capitalize-and-filter');
 
+const strings = ['apple', 'napkin', 'fire']
+
 describe('capitalizeAndFilter', () => {
   it('it should capitalize every string', () => {
-    
+    const expected = ['APPLE', 'NAPKIN', 'FIRE'];
+    const actual = capitalizeAndFilter(strings);
+
+    expect(actual).toEqual(expected);
   });
 });
